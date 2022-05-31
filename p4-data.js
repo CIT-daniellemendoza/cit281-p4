297f30cea97d74cdabfc42919171b08987721649
daniellemendoza <67397853+daniellemendoza@users.noreply.github.com>
2022-05-31 11:56:16 -0700

    Create p4-data.js

---
+++ p4-data.js
@@ -0,0 +1,27 @@
+/*
+  CIT 281 Project 4
+  Danielle Mendzoza
+*/
+
+// Part : Create data file (copy from project assignment)
+
+// Question and answer data array
+const data = [
+    {
+      question: "Q1",
+      answer: "A1",
+    },
+    {
+      question: "Q2",
+      answer: "A2",
+    },
+    {
+      question: "Q3",
+      answer: "A3",
+    },
+  ];
+  
+  // Export statement must be below data declaration - no hoisting with const
+  module.exports = {
+    data,
+  };
